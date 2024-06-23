5a400b559b24ab75dc4b8b69be23d88effdf9b00
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-06-23 14:35:57 +0530

    deployment changes one

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import initiativeRoutes from "./routes/initiative.route.js";
 import cookieParser from "cookie-parser";
+import path from path;
 
 dotenv.config();
 import cors from "cors";
@@ -12,6 +13,8 @@ import cors from "cors";
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("MongoDb is connected");
 });
+
+const __dirname = path.resolve()
 
 const app = express();
 
@@ -26,6 +29,12 @@ app.listen(3000, () => {
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/initiative", initiativeRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statuCode || 500;
